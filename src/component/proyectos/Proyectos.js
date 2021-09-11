78cc26c5b35b78269cd50e44bfc7121d9be4c83c
Negro <fabian_1244@hotmail.com>
2021-09-11 00:22:18 -0300

    Feature: Imagenes agregadas

--- src/component/proyectos/Proyectos.js
+++ src/component/proyectos/Proyectos.js
@@ -6,24 +6,34 @@ const Proyectos = () => {
     return (
         <div className="container p-5">
             <div className="row">
-                <div className="col-lg-4 bg-secondary">
+                <div className="col-lg-4 text-center">
                     <h1>Titulo</h1>
                     <p>Hola</p>
                     <img className="img-fluid" src={prueba}/>
-
+                    <button type="button" className="btn btn-primary mt-2">Demo</button>
                 </div>
 
-                <div className=" col-lg-4 bg-secondary">
+                <div className=" col-lg-4 text-center">
                     <h1>Titulo</h1>
                     <p>Hola</p>
                     <img className="img-fluid" src={prueba}/>
+                    <button type="button" className="btn btn-primary mt-2">Demo</button>
                 </div>
                 
-                <div className="col-lg-4 bg-secondary">
+                <div className="col-lg-4 text-center">
                     <h1>Titulo</h1>
                     <p>Hola</p>
                     <img className="img-fluid" src={prueba}/>
+                    <button type="button" className="btn btn-primary mt-2">Demo</button>
                 </div>
+                <div className="col-lg-4 text-center mt-5"></div>
+                <div className="col-lg-4 text-center mt-5">
+                    <h1>Titulo</h1>
+                    <p>Hola</p>
+                    <img className="img-fluid" src={prueba}/>
+                    <button type="button" className="btn btn-primary mt-2">Demo</button>
+                </div>
+                <div className="col-lg-4 text-center mt-5"></div>
             </div>
             
         </div>
