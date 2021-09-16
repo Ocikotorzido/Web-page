6337db8d1d1239de5c0f81f52a39f16308270fa5
Negro <fabian_1244@hotmail.com>
2021-09-16 00:00:57 -0300

    Refactor: cambio tamaño logo github

--- src/component/proyectos/Proyectos.js
+++ src/component/proyectos/Proyectos.js
@@ -15,38 +15,38 @@ const Proyectos = () => {
         <div className="container p-5">
             <div className="row">
                 <div className="col-lg-4 text-center">
-                    <h1>Titulo</h1>
+                    <h1 className="text-dark">Titulo</h1>
                     <p>Hola</p>
                     <img className="img-fluid" src={congenial}/>
                     <a href="https://github.com/Ocikotorzido/Web-page">
-                        <img className="img-fluid logo mt-2" src={git}/>    
+                        <img className="bloque img-fluid logo mt-2" src={git}/>    
                     </a>                  
                 </div>
 
                 <div className=" col-lg-4 text-center">
-                    <h1>Titulo</h1>
+                    <h1 className="text-dark">Titulo</h1>
                     <p>Hola</p>
                     <img className="img-fluid" src={facial}/>
                     <a href="https://github.com/Ocikotorzido/Face">
-                        <img className="img-fluid logo mt-2" src={git}/>    
+                        <img className="bloque img-fluid logo mt-2" src={git}/>    
                     </a>           
                 </div>
                     
                 <div className="col-lg-4 text-center">
-                    <h1>Titulo</h1>
+                    <h1 className="text-dark">Titulo</h1>
                     <p>Hola</p>
                     <img className="img-fluid" src={neural}/>
                     <a href="https://github.com/Ocikotorzido/IA">
-                        <img className="img-fluid logo mt-2" src={git}/>    
+                        <img className="bloque img-fluid logo mt-2" src={git}/>    
                     </a>                  
                 </div>
                 <div className="col-lg-4 text-center mt-5"></div>
                 <div className="col-lg-4 text-center mt-5">
-                    <h1>Titulo</h1>
+                    <h1 className="text-dark">Titulo</h1>
                     <p>Hola</p>
                     <img className="img-fluid" src={prueba}/>
                     <Link to="">
-                        <img className="img-fluid logo mt-2" src={git}/>    
+                        <img className="bloque img-fluid logo mt-2" src={git}/>    
                     </Link>       
                 </div>
                 <div className="col-lg-4 text-center mt-5"></div>
