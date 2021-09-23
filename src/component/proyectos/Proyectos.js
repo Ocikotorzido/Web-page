8c2f276db4b6f43ad7e8ae8fb29093cf5d0285e5
Negro <fabian_1244@hotmail.com>
2021-09-23 16:58:35 -0300

    cambios en tarjetas

--- src/component/proyectos/Proyectos.js
+++ src/component/proyectos/Proyectos.js
@@ -9,7 +9,7 @@ import { Link, Redirect } from 'react-router-dom';
 const Proyectos = () => {
 
     return (
-        <>
+        <div className="">
         <div className="text-center bg-dark fs-1 text-white altura d-flex justify-content-center align-items-center">Proyectos</div>
         <div class="card-group p-5">
         <div class="card">
@@ -18,7 +18,7 @@ const Proyectos = () => {
             <h5 class="card-title">Taller Mecanico</h5>
             <p class="card-text">Sistema de gestión y administración web del taller mecánico</p>
             <div className="d-flex justify-content-center">
-                <button type="button" class="btn btn-primary">Primary</button>
+                <button type="button" class="btn btn-primary">Info</button>
                 <img className="logo img-fluid" src={git}/>
             </div>
             </div>
@@ -27,9 +27,9 @@ const Proyectos = () => {
             <img class="card-img-top" src={mecanica} alt="Card image cap"/>
             <div class="card-body">
             <h5 class="card-title">Reconocimiento Facial</h5>
-            <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+            <p class="card-text">Software de reconocimiento facial </p>
             <div className="d-flex justify-content-center">
-                <button type="button" class="btn btn-primary">Primary</button>
+                <button type="button" class="btn btn-primary">Info</button>
                 <img className="logo img-fluid" src={git}/>
             </div>
             </div>
@@ -38,15 +38,24 @@ const Proyectos = () => {
             <img class="card-img-top" src={mecanica} alt="Card image cap"/>
             <div class="card-body">
             <h5 class="card-title">Water-Pot</h5>
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+            <p class="card-text">Sitio web que permitirá adquirir un dispositivo para ahorrar agua en cosechas, además de incorporar un llamado a la acción.</p>
             <div className="d-flex justify-content-center">
-                <button type="button" class="btn btn-primary">Primary</button>
+                <button type="button" class="btn btn-primary">Info</button>
                 <img className="logo img-fluid" src={git}/>
             </div>
             </div>
         </div>
         </div>
-        </>
+        <div className="text-center bg-dark fs-1 text-white altura d-flex justify-content-center align-items-center">Proyectos en curso</div>
+        <div class="card" >
+        <img src={mecanica} class="card-img-top" alt="."/>
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+        </div>
+        </div>
     )
 }
 
