17f5090c05e2812e036cd5fbbad6cf9a2539f931
Negro <fabian_1244@hotmail.com>
2021-09-28 16:43:34 -0300

    typed agregado

--- src/component/cover/Cover.js
+++ src/component/cover/Cover.js
@@ -9,6 +9,14 @@ const Cover = () => {
         <div className="cover-container text-center mt-5">
             <video className="video" src={planeta} autoPlay loop muted />
             <h1>Intelligence</h1>
+            <Typed className="letras text-white d-flex justify-content-center"
+                    strings={[
+                        'GitHub: Ocikotorzido',
+                        'Instagram: Tio_chokita']}
+                    typeSpeed={50}
+                    backSpeed={50}
+                    loop>
+                </Typed>
             
         </div>
     )
